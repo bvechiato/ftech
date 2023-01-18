04d61ffec982926f165df86824f8cd60ead7da5a
bvechiato <beaaccounts@icloud.com>
2023-01-18 16:41:56 +0000

    Trying useState

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,22 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+
+const ChangeColour = () => {
+    
+}
+
 
 function Nav() {
+    const [clicked, setClicked] = useState(false);
+
+
   return (
     <nav className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
         <ul>
             <ol className="flex">
-                <li className="mx-5">
-                    <Link to="/pages/PC01">PCO1</Link>
+                <li id="pc01" className="mx-5">
+                    <Link onClick={() => ChangeColour} to="/pages/PC01">PCO1</Link>
                 </li>
-                
-                <li className="mx-5">
+                <li id="pc02" className="mx-5">
                     <Link to="/pages/PC02">PCO2</Link>
                 </li>
             </ol>
---
+++ src/components/NavItem.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+function NavItem() {
+  return (
+    <div>NavItem</div>
+  )
+}
+
+export default NavItem
