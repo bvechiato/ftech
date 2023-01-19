ba6c9352906987218c26681bc972e8cc6a207cb2
bvechiato <beaaccounts@icloud.com>
2023-01-19 16:02:44 +0000

    Definitions look better

--- src/components/Term.js
+++ src/components/Term.js
@@ -24,7 +24,9 @@ function Term({ course, term }) {
     return (
         <div className="container mx-auto bg-gray-100 rounded-xl p-8 m-10 pb-6">
             <p className="text-lg pb-4 font-semibold text-gray-700">{ term }</p>
+            <div className="grid grid-cols-4 gap-4 ">
             {terms.map((definition, index) => <Definition key={index} course={course} term={term} definition={definition}/>)}
+            </div>
         </div>
     )
 }
