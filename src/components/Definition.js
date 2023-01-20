d67309a4e908abbd7dd1b00b9398b229794cade0
bvechiato <beaaccounts@icloud.com>
2023-01-20 19:17:52 +0000

    Styling

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -21,7 +21,7 @@ function Definition({ course, term, definition }) {
     return (
         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100">
             <h5 className="mb-2 text-l font-bold tracking-tight text-gray-900">{definition}</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">{definitionExp}</p>
+            <p className="font-normal text-gray-700">{definitionExp}</p>
         </div>
     )
 }
